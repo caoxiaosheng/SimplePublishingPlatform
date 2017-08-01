9a02af76300466f8ba295442b7274bc4290563ac
caoxiaosheng <836900367@qq.com>
2017-08-02 00:20:51 +0800

    修改了发布新版本跳转

---
+++ SimplePublishingPlatform/Scripts/myscrips/AddRepertory.js
@@ -0,0 +1,25 @@
+﻿function addRepertory() {
+    var repertoryName = $.trim($('#repertoryName').val());
+    if (repertoryName === "") {
+        alert("请输入版本名称");
+        return false;
+    }
+    $.ajax({
+        type:"post",
+        url: "/Publish/Add",
+        data: { "repertoryName": repertoryName },
+        beforeSend:function() {
+            $('#addRepertoryProgress').show();
+        },
+        success:function() {
+            
+        },
+        error: function (xmlHttpRequest, textStatus, errorThrown) {
+            alert("错误\r"+textStatus);
+        },
+        complete:function() {
+            $('#addRepertoryProgress').hide();
+        }
+    });
+
+}
